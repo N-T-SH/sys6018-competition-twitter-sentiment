eca33891d44d348e995958b80d1a904a3c31d642
Court Haworth <courthaworth@Courts-MBP.zyxel.com>
2017-09-20 19:52:49 -0400

    Made a small change

--- Model.R
+++ Model.R
@@ -3,9 +3,9 @@ library(readr)
 
 train <- read_csv("train.csv")
 test  <-  read_csv("test.csv")
+
 g <- function(train){
   
-
 tweets = VCorpus(DataframeSource(train))
 inspect(tweets[1:2])
 tweets[[1]]$content
@@ -32,7 +32,7 @@ return(as.matrix(tweets.clean.tfidf))
 tweets.90 = g(train)
 
 
-tweets.90 = removeSparseTerms(tweets.clean.tfidf, 0.99)  # remove terms that are absent from at least 99% of documents (keep most terms)
+tweets.90 = removeSparseTerms(tweets.90, 0.99)  # remove terms that are absent from at least 99% of documents (keep most terms)
 tweets.90
 tweets.train = as.data.frame(as.matrix(tweets.90))
 train.new = cbind(tweets.train,train$sentiment)
@@ -44,8 +44,6 @@ train.new$januari <-  NULL
 train.new$univers <-  NULL
 
 model2 <- lm(`train$sentiment`~.,data = train.new)
-install.packages("rJava")
-library(RWeka)
 test.90 = test.90[colnames(test.90) %in% colnames(train.new)]
 train.new2 = train.new
 train.new2$`train$sentiment` = NULL

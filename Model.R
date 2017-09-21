0243720c2d358bed81656ab662e93ac3cf0f7a76
Court Haworth <courthaworth@Courts-MBP.zyxel.com>
2017-09-20 20:07:58 -0400

    Added Cross validation to linear model and improved variable selection

--- Model.R
+++ Model.R
@@ -1,6 +1,7 @@
 
 library(tm)
 library(readr)
+library(boot)
 
 train <- read_csv("train.csv")
 test  <-  read_csv("test.csv")
@@ -44,7 +45,14 @@ train.new$fbi <-  NULL
 train.new$januari <-  NULL
 train.new$univers <-  NULL
 
-model2 <- lm(`train$sentiment`~.,data = train.new)
+model2 <- glm(`train$sentiment`~cant+car+come+dont+excit+googl+insur+less+need+
+                safer+save+soon+thing+want+wait+warn+wrong,data = train.new)
+summary(model2)
+cv.error = cv.glm(train.new,model2)$delta[1] #[1] 0.6508767 0.6507858 with all variables
+#0.5535677 with 20 best variables
+#0.5529536 with 17 best variables, so for now, use just these variables
+
+
 install.packages("RWeka")
 library(RWeka)
 test.90 = test.90[colnames(test.90) %in% colnames(train.new)]
@@ -52,9 +60,11 @@ test.90 = test.90[colnames(test.90) %in% colnames(train.new)]
 # train.label <- train.new2$`train$sentiment`
 # train.new2$`train$sentiment` = NULL
 model3 <-  IBk(`train$sentiment`~.,data = train.new, control = Weka_control(K = 15, X = TRUE))
-summary(model3)
+summary(model2)
 
-guesses  = predict(model3,test.90)
+
+
+guesses  = predict(model2,test.90)
 guesses = round(guesses,digits = 0)
 guesses = cbind(test$id,guesses)
 colnames(guesses) <- c("id","sentiment")
